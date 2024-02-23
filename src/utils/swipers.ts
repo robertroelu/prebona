81fa92bd35a8ad56782f3a1942d9cb679ffa2cd6
Róbert Klimant <roboklimant@gmail.com>
2024-02-23 14:05:36 +0100

    feat: added swipers

--- src/utils/swipers.ts
+++ src/utils/swipers.ts
@@ -5,4 +5,41 @@ import { Autoplay, EffectFade } from 'swiper/modules';
 
 export const swipers = () => {
   const swiperModules = [Autoplay, EffectFade];
+
+  const swipersEl = document.querySelectorAll('[swiper-option]') as NodeListOf<HTMLElement>;
+  if (!swipersEl) return;
+
+  swipersEl.forEach((el) => {
+    const elAttr = el.getAttribute('swiper-option');
+    let settings = {};
+    switch (elAttr) {
+      case 'areas':
+        settings = {
+          slidesPerView: 3,
+          spaceBetween: 32,
+        };
+        break;
+      case 'testimonial': {
+        settings = {
+          slidesPerView: 1.5,
+          spaceBetween: 32,
+        };
+        break;
+      }
+      case 'blog': {
+        settings = {
+          slidesPerView: 3,
+          spaceBetween: 32,
+        };
+      }
+      case 'products': {
+        settings = {
+          slidesPerView: 3,
+          spaceBetween: 32,
+        };
+      }
+    }
+
+    const swiper = new Swiper(el, settings);
+  });
 };
