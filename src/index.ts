26e0c52496a7546d8be72de379465284f2ace254
Róbert Klimant <roboklimant@gmail.com>
2024-03-21 15:28:28 +0100

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //Finsweet attributes
-// import { linkblockedit } from '@finsweet/attributes-linkblockedit/';
+import { linkblockedit } from '@finsweet/attributes-linkblockedit/';
 
 //Modal
 // import { modal } from '$modal/modal';
@@ -8,7 +8,7 @@
 // import { nestedElement } from './nest/nestElement';
 
 //Utils
-// import { swipers } from '$utils/swipers';
+import { swipers } from '$utils/swipers';
 
 // console.log = function () {};
 
@@ -16,32 +16,7 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   // nestedElement();
   // modal();
-  // linkblockedit();
-  // swipers();
 
-  const bodyAttr = document.querySelector('body') as HTMLElement;
-
-  if (localStorage.getItem('switch') === 'dark') {
-    bodyAttr.setAttribute('mode', 'dark');
-  } else {
-    bodyAttr.setAttribute('mode', 'light');
-  }
-
-  setTimeout(() => {
-    const elLogo = document.querySelector('.logo_embed') as HTMLElement;
-    if (!elLogo) return;
-    elLogo.style.color = 'var(--mode-1--backgroundcolor--bgc1)';
-  }, 1700);
-
-  const switchEl = document.querySelector('[switcher]');
-  switchEl?.addEventListener('click', (e) => {
-    const storageSwitch = localStorage.getItem('switch');
-    if (storageSwitch === 'light') {
-      localStorage.setItem('switch', 'dark');
-      bodyAttr.setAttribute('mode', 'dark');
-    } else {
-      localStorage.setItem('switch', 'light');
-      bodyAttr.setAttribute('mode', 'light');
-    }
-  });
+  linkblockedit();
+  swipers();
 });
